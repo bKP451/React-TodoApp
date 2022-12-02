e4762d4fece3ed5a014bd0f3a6685bffb0ecd980
Pandey Bikash <bkppandey87@gmail.com>
2022-12-03 03:24:21 +0545

    React interactivity:Editing, filtering and conditional rendering

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: (task) => !task.completed,
+  Completed: (task) => task.completed
+}
+
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
 function App(props) {
   
-  const [tasks, setTasks] = useState(props.tasks)
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+
 
   function addTask(name){
     const newTask = {id:`todo-${nanoid()}`, name, completed:false};
     setTasks([...tasks, newTask]);
   }
-
 
   function toggleTaskCompleted(id) {
     const updatedTasks = tasks.map((task)=>{
@@ -28,12 +39,27 @@ function App(props) {
     setTasks(updatedTasks);
   }
 
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map((task)=>{
+      // if this task has the same ID as the edited task
+      if (id === task.id){
+        return {...task, name:newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+
+
   function deleteTask(id){
     const remainingTasks = tasks.filter((task)=>id !== task.id);
     setTasks(remainingTasks);
   }
 
-  const taskList = tasks.map((task) =>
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map((task) =>
    <Todo 
     id={task.id} 
     name={task.name} 
@@ -41,7 +67,17 @@ function App(props) {
     key={task.id}
     toggleTaskCompleted = {toggleTaskCompleted}
     deleteTask = {deleteTask}
+    editTask = {editTask}
   />);
+
+  const filterList = FILTER_NAMES.map((name)=>(
+    <FilterButton
+     key={name} 
+     name={name}
+     isPressed = {name === filter}
+     setFilter = {setFilter}
+     />
+  ))
 
   // If there is a single task, than task else tasks
   const tasksNoun = (taskList.length !==1 )?("tasks"):("task");
@@ -51,9 +87,7 @@ function App(props) {
       <h1>Pandey's tasks</h1>
       <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
        {remainingTasksHeading}
