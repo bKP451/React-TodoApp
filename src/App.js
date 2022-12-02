f401e7e13618517f93136e181d811d2865a93c1e
Pandey Bikash <bkppandey87@gmail.com>
2022-12-02 11:30:55 +0545

    React interactivity: Events and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
+import React, {useState} from "react";
+import { nanoid } from "nanoid";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks.map((task) => <Todo 
+  
+  const [tasks, setTasks] = useState(props.tasks)
+
+  function addTask(name){
+    const newTask = {id:`todo-${nanoid()}`, name, completed:false};
+    setTasks([...tasks, newTask]);
+  }
+
+
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map((task)=>{
+      
+      // if this task has the same id as the edited task
+      if(id === task.id){
+        // use spread operator to make a new object
+        // whose completed prop has been inverted
+        return {...task, completed:!task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id){
+    const remainingTasks = tasks.filter((task)=>id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map((task) =>
+   <Todo 
     id={task.id} 
     name={task.name} 
     completed={task.completed} 
     key={task.id}
+    toggleTaskCompleted = {toggleTaskCompleted}
+    deleteTask = {deleteTask}
   />);
 
+  // If there is a single task, than task else tasks
+  const tasksNoun = (taskList.length !==1 )?("tasks"):("task");
+  const remainingTasksHeading = `${taskList.length} ${tasksNoun} Remaining !!`
   return (
     <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form />
+      <h1>Pandey's tasks</h1>
+      <Form onSubmit={addTask} />
       <div className="filters btn-group stack-exception">
         <FilterButton />
         <FilterButton />
         <FilterButton />
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+       {remainingTasksHeading}
       </h2>
       <ul
         role="list"
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 function Form(props) {
+
+    const [name, setName] = useState('');
+
+    function handleChange(e) {
+        setName(e.target.value);
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        // If the input box is non-empty cannot add the task
+        if(name){
+            props.onSubmit(name);
+        }
+            
+        setName('');
+    }
     return(
-        <form>
+        <form onSubmit={handleSubmit}>
         <h2 className="label-wrapper">
           <label htmlFor="new-todo-input" className="label__lg">
             What needs to be done?
@@ -15,7 +30,10 @@ function Form(props) {
           className="input input__lg"
           name="text"
           autoComplete="off"
+          value = {name}
+          onChange={handleChange}
         />
+
         <button type="submit" className="btn btn__primary btn__lg">
           Add
         </button>
